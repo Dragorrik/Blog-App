537abb556bcbc9e0c102ddf9ffa209d87d6cbdd8
Md Aarik Enam <aarikanan@gmail.com>
2025-01-08 12:40:35 +0600

    updated app pages

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,8 +2,6 @@ import 'package:get/get.dart';
 
 import '../modules/admin_home/bindings/admin_home_binding.dart';
 import '../modules/admin_home/views/admin_home_view.dart';
-import '../modules/home/bindings/home_binding.dart';
-import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
 import '../modules/register/bindings/register_binding.dart';
@@ -16,16 +14,10 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const HOME = Routes.HOME;
   static const LOGIN = Routes.LOGIN;
   static const REGISTER = Routes.REGISTER;
 
   static final routes = [
-    GetPage(
-      name: _Paths.HOME,
-      page: () => const HomeView(),
-      binding: HomeBinding(),
-    ),
     GetPage(
       name: _Paths.LOGIN,
       page: () => LoginView(),
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,7 +3,6 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   Routes._();
-  static const HOME = _Paths.HOME;
   static const LOGIN = _Paths.LOGIN;
   static const REGISTER = _Paths.REGISTER;
   static const ADMIN_HOME = _Paths.ADMIN_HOME;
@@ -12,7 +11,6 @@ abstract class Routes {
 
 abstract class _Paths {
   _Paths._();
-  static const HOME = '/home';
   static const LOGIN = '/login';
   static const REGISTER = '/register';
   static const ADMIN_HOME = '/admin-home';
